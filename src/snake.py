c84525588d24bf40d94d424e08c7b8e905f3e756
Mizhab A S <mizhabayyaril@gmail.com>
2026-03-03 10:16:46 +0530

    Initial commit

---
+++ src/snake.py
@@ -0,0 +1,35 @@
+import pygame
+import random
+
+WIDTH, HEIGHT = 600, 400
+BLOCK = 20
+
+class SnakeGame:
+    def __init__(self):
+        self.snake = [(100, 100)]
+        self.direction = "RIGHT"
+        self.food = self.spawn_food()
+
+    def spawn_food(self):
+        return (random.randrange(0, WIDTH, BLOCK),
+                random.randrange(0, HEIGHT, BLOCK))
+
+    def move(self):
+        head_x, head_y = self.snake[0]
+
+        if self.direction == "UP":
+            head_y -= BLOCK
+        elif self.direction == "DOWN":
+            head_y += BLOCK
+        elif self.direction == "LEFT":
+            head_x -= BLOCK
+        elif self.direction == "RIGHT":
+            head_x += BLOCK
+
+        new_head = (head_x, head_y)
+        self.snake.insert(0, new_head)
+
+        if new_head == self.food:
+            self.food = self.spawn_food()
+        else:
+            self.snake.pop()
